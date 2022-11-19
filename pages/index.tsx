32893922da70db7baf79fa5b5b676645681dd3fa
ian <ian@romeblockchain.com>
2022-11-19 12:25:56 +0800

    setup twinmacro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="text-3xl font-bold underline bg-red-500">adfa</div>;
+  return <div tw="text-3xl font-bold underline bg-red-500">adfa</div>;
 }
